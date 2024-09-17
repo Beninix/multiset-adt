28e009aa1f9c25e685eded70d3bfb0d384093443
Beninix <brandon080808@outlook.com>
2024-09-16 20:44:32 -0400

    fill MultiSet.java

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -2,13 +2,34 @@
  * Abstract class representing a MultiSet ADT.
  */
 public abstract class MultiSet {
-
     /**
      * Add the given item to this multiset.
      *
      * @param item the item to add
      */
     abstract void add(Integer item);
-
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+    /**
+     * Remove the given item from this multiset.
+     *
+     * @param item the item to remove
+     */
+    abstract void remove(Integer item);
+    /**
+     * Check if the given item is in the multiset.
+     *
+     * @param item the item to check
+     */
+    abstract boolean contains(Integer item);
+    /**
+     * Check if the multiset is empty.
+     */
+    abstract boolean is_empty(Integer item);
+    /**
+     * Count how many times the given element is in the multiset.
+     */
+    abstract int count();
+    /**
+     * Count how many elements are in the multiset.
+     */
+    abstract int size();
 }
